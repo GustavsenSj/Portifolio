b82dde67909a18259461b0962ba4c0d56a24952c
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-07 13:54:50 +0100

    try scroll animation fade out

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <p>Home</p>
+      <p>About</p>
+      <p>Projects</p>
+      <p>Contact</p>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Hero from "./components/hero/hero.jsx";
+import Navbar from "./components/navbar/navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <div className="scroll-mask"></div> */}
+    <Navbar />
     <Hero />
   </React.StrictMode>
 );
