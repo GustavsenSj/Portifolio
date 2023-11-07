ceacc6e508c040fa96776158b69da0bc1d088fae
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-07 14:15:54 +0100

    working fade out effect

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,23 +1,12 @@
 import "./hero.css";
-import { useState, useEffect } from "react";
 function Hero() {
-  const [scroll, setScroll] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 50);
-      console.log(window.scrollY);
-    });
-  }, []);
-
   return (
     <div className="hero-container">
-      <div className="hero-text">
-        <h1 className={scroll ? "fade-out" : "fade-inn"}>Hi, and welcome!</h1>
-        <h1 className={scroll ? "fade-out" : "fade-inn"}>
-          My name is Sjur and i am a full-stack developer
-        </h1>
+      <div className="hero-text fade-out">
+        <h1>Hi, and welcome!</h1>
+        <h1>My name is Sjur and i am a full-stack developer</h1>
       </div>
-      <div className={`${"contact-me"} ${scroll ? "fade-out" : "fade-in"}`}>
+      <div className="contact-me fade-out">
         <p>Contact me</p>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Navbar from "./components/navbar/navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <div className="scroll-mask"></div> */}
     <Navbar />
     <Hero />
   </React.StrictMode>
