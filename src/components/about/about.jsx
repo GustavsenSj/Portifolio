d7a54d86c2764edd5ba165d92ef235a76fc55a83
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-08 10:58:47 +0100

    about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,20 +1,26 @@
 import "./About.css";
+import sjur from "../../assets/sjur.png";
 function About() {
   return (
-    <div className="about-container">
-      <h2 className="fade">About me:</h2>
-      <p className="fade">
-        Sjur Gustavsen is an enthusiastic and eager developer. He has a strong
-        desire to continue to grow himself both as a developer and as an
-        individual. Sjur has always been interested in technology and enjoys
-        keeping up with new technologies. He recently completed a
-        bachelor&apos;s degree in computer science with a specialization in
-        application development at NTNU. Here, he has had the opportunity to
-        work with a wide range of technologies and work methodologies, both
-        independently and as part of a team. In addition to the core subjects in
-        his studies, Sjur chose elective courses in Mobile Applications,
-        Computer Graphics, Game Development, and Internship.{" "}
-      </p>
+    <div className="about-page">
+      <div className="about-container">
+        <h2 className="fade">About me:</h2>
+        <p className="fade">
+          I am an enthusiastic and eager developer. Whom has strong desire to
+          continue and grow myself both as a developer and as an individual. I
+          Have always been interested in technology and enjoys to keep myself up
+          to date with new technologies. I recently completed a bachelor&apos;s
+          degree in computer science with a specialization in application
+          development at NTNU. Here, I had the opportunity to work with a wide
+          range of technologies and work methodologies, both independently and
+          as part of a team. In addition to the core subjects of the studies, I
+          had elective courses in Mobile Applications, Computer Graphics, Game
+          Development, and Internship.
+        </p>
+      </div>
+      <div className="image-container fade">
+        <img src={sjur} alt="Image of Sjur" />
+      </div>
     </div>
   );
 }
