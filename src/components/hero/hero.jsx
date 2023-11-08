e28daf6fccde63c52416d802e58d3a25fa9b2747
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-08 09:25:19 +0100

    typing animation

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,8 +3,12 @@ function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-text fade">
-        <h1>Hi, and welcome!</h1>
-        <h1>My name is Sjur and i am a full-stack developer</h1>
+        <h1 className="line1">
+          Hi, and welcome! <br />
+        </h1>
+        <h1 className="line2">
+          My name is Sjur and i am a full-stack developer
+        </h1>
       </div>
       <div className="contact-me fade">
         <p>Contact me</p>
