de3023712aecb166fde381e4da727818f2a8bff0
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-09 11:01:46 +0100

    warning for browser suport

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,48 +6,63 @@ function Projects() {
         <h1 className="fade title">Projects</h1>
         <div className="project-container">
           <h2 className="fade">MeFit</h2>
-          <p className="fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
-            voluptates non quos a quibusdam eaque quam perspiciatis quidem et
-            culpa aliquam iste voluptas quisquam enim, animi dicta tempora
-            vitae?
-          </p>
+          <div className="project-content fade">
+            <div className="image-container-blob image-blob-background"></div>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
+              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
+              tempora vitae?
+            </p>
+          </div>
         </div>
         <div className="project-container">
           <h2 className="fade">Marine Traffic portal</h2>
-          <p className="fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
-            voluptates non quos a quibusdam eaque quam perspiciatis quidem et
-            culpa aliquam iste voluptas quisquam enim, animi dicta tempora
-            vitae?
-          </p>
+          <div className="project-content fade">
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
+              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
+              tempora vitae?
+            </p>
+            <div className="image-container-blob image-blob-background"></div>
+          </div>
         </div>
         <div className="project-container">
           <h2 className="fade">City simulator</h2>
-          <p className="fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
-            voluptates non quos a quibusdam eaque quam perspiciatis quidem et
-            culpa aliquam iste voluptas quisquam enim, animi dicta tempora
-            vitae?
-          </p>
+          <div className="project-content fade">
+            <div className="image-container-blob image-blob-background"></div>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
+              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
+              tempora vitae?
+            </p>
+          </div>
         </div>
         <div className="project-container">
           <h2 className="fade">Booking system mobile app</h2>
-          <p className="fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
-            voluptates non quos a quibusdam eaque quam perspiciatis quidem et
-            culpa aliquam iste voluptas quisquam enim, animi dicta tempora
-            vitae?
-          </p>
+          <div className="project-content fade">
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
+              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
+              tempora vitae?
+            </p>
+            <div className="image-container-blob image-blob-background"></div>
+          </div>
         </div>
         <div className="project-container">
           <h2 className="fade">Spot price dashboard</h2>
-          <p className="fade">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat
-            voluptates non quos a quibusdam eaque quam perspiciatis quidem et
-            culpa aliquam iste voluptas quisquam enim, animi dicta tempora
-            vitae?
-          </p>
+          <div className="project-content fade">
+            <div className="image-container-blob image-blob-background"></div>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
+              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
+              tempora vitae?
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,19 @@ import Hero from "./components/hero/hero.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
 import About from "./components/about/about.jsx";
 import Projects from "./components/projects/projects.jsx";
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
+    <div className="warning">
+      <p>
+        ⚠️ Your browser does not support Scroll-Linked Animations. This website
+        might not work as intended. To cater for this a{" "}
+        <a href="https://github.com/flackr/scroll-timeline">polyfill</a> has
+        been loaded. ⚠️
+      </p>
+    </div>
     <Hero />
     <About />
     <Projects />
