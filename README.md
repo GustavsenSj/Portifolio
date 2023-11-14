a01d7ab0c8b06ddf3f54774bb5bff9c5de4a9f84
Sjur G <sjur.gustavsen@no.experis.com>
2023-11-14 15:24:02 +0100

    readme and small style fixes

--- README.md
+++ README.md
@@ -6,3 +6,42 @@ This is my portfolio site. It's a hobby project to learn more about css animatio
 
 - The animated "blobs" in the background is from @LA_water link to [codepen](https://codepen.io/LA_water/pen/rNaYZBb)
 - The pages uses ScollTimline and as most browser does not support this (as of 09/11/23) a polyfill is used to make it work in most browsers. The polyfill is from [Scroll-timeline Polyfill](https://github.com/flackr/scroll-timeline)
+
+
+## How to setup the project on your local machine
+The project uses [Firestore](https://console.firebase.google.com/) for storage. To setup the project on your local machine you need to create a file called `firebase.js` in the `src` folder. The file should look like this:
+
+```javascript
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration 
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+```
+
+You also need to create a `.env` file in the root folder. 
+All the values you will find in your firebase console project settings.
+The file should look like this: 
+
+```
+VITE_REACT_APP_API_KEY= your api key
+VITE_REACT_APP_AUTH_DOMAIN= your auth domain
+VITE_REACT_APP_PROJECT_ID= your project id
+VITE_REACT_APP_STORAGE_BUCKET= your storage bucket
+VITE_REACT_APP_MESSAGING_SENDER_ID= your messaging sender id
+VITE_REACT_APP_APP_ID= your app id
+VITE_REACT_APP_MEASUREMENT_ID= your measurement id
+```
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -45,54 +45,6 @@ function Projects() {
             </div>
           </div>
         ))}
-        <div className="project-container">
-          <h2 className="fade">Marine Traffic portal</h2>
-          <div className="project-content fade">
-            <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
-              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
-              tempora vitae?
-            </p>
-            <div className="image-container-blob image-blob-background"></div>
-          </div>
-        </div>
-        <div className="project-container">
-          <h2 className="fade">City simulator</h2>
-          <div className="project-content fade">
-            <div className="image-container-blob image-blob-background"></div>
-            <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
-              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
-              tempora vitae?
-            </p>
-          </div>
-        </div>
-        <div className="project-container">
-          <h2 className="fade">Booking system mobile app</h2>
-          <div className="project-content fade">
-            <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
-              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
-              tempora vitae?
-            </p>
-            <div className="image-container-blob image-blob-background"></div>
-          </div>
-        </div>
-        <div className="project-container">
-          <h2 className="fade">Spot price dashboard</h2>
-          <div className="project-content fade">
-            <div className="image-container-blob image-blob-background"></div>
-            <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-              quaerat voluptates non quos a quibusdam eaque quam perspiciatis
-              quidem et culpa aliquam iste voluptas quisquam enim, animi dicta
-              tempora vitae?
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   );
